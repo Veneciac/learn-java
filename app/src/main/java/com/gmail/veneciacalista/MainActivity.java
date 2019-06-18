076cdd241a5c209267ffe92c601db8765d85ae29
Veneciac <veneciacalista00@gmail.com>
2019-06-18 15:23:12 +0700

    app

---
+++ app/src/main/java/com/gmail/veneciacalista/MainActivity.java
@@ -0,0 +1,85 @@
+package com.gmail.veneciacalista;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    ArrayList<String> arrayList = new ArrayList<>();
+
+    public static String BaseUrl = "https://api.themoviedb.org/3/";
+    private TextView textViewResult;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewResult = findViewById(R.id.text_view_result);
+
+        listView = (ListView)findViewById(R.id.listview);
+
+//        arrayList.add("hahaha");
+
+        getMovie();
+
+    }
+
+    private void getMovie() {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BaseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        MovieApi service = retrofit.create(MovieApi.class);
+        Call<MovieResponse> call = service.getMovie();
+
+        call.enqueue(new Callback<MovieResponse>() {
+
+            @Override
+            public void onResponse(@NonNull Call<MovieResponse> call, @NonNull Response<MovieResponse> response) {
+                if (response.code() == 200) {
+                    MovieResponse movieResponse = response.body();
+                    assert movieResponse != null;
+
+//                    String stringBuilder = "--->" + movieResponse.results.get(0).getTitle();
+//
+//                    arrayList.add(stringBuilder);
+
+                    for (Movie movie : movieResponse.results) {
+                        arrayList.add(movie.getTitle());
+                    }
+//                    textViewResult.setText(stringBuilder);
+
+                    ArrayAdapter arrayAdapter=new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, arrayList);
+                    listView.setAdapter(arrayAdapter);
+
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<MovieResponse> call, @NonNull Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+
+        });
+
+    }
+}
---
+++ app/src/main/java/com/gmail/veneciacalista/MovieApi.java
@@ -0,0 +1,9 @@
+package com.gmail.veneciacalista;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface MovieApi {
+    @GET("movie/now_playing?api_key=d94b3ad2730dee34212c8f50fb87a861&page=1")
+    Call<MovieResponse> getMovie();
+}
