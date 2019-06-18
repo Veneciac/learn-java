73ccd6c7fb99aa280ea42741342aa5d0cae67148
Veneciac <veneciacalista00@gmail.com>
2019-06-18 15:25:36 +0700

    remove api key

--- app/src/main/java/com/gmail/veneciacalista/MovieApi.java
+++ app/src/main/java/com/gmail/veneciacalista/MovieApi.java
@@ -4,6 +4,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface MovieApi {
-    @GET("movie/now_playing?api_key=d94b3ad2730dee34212c8f50fb87a861&page=1")
+    @GET("movie/now_playing")
     Call<MovieResponse> getMovie();
 }
