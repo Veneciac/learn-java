1d641f442c1ce24b2a515f5bf7347617125079bf
Veneciac <veneciacalista00@gmail.com>
2019-07-03 17:37:30 +0700

    fetch from firebase

--- app/src/main/java/com/gmail/veneciacalista/firebase/response/Genre.java
+++ app/src/main/java/com/gmail/veneciacalista/firebase/response/Genre.java
@@ -1,5 +1,5 @@
 package com.gmail.veneciacalista.firebase.response;
 
-public class Menu {
+public class Genre {
 
 }
--- app/src/main/java/com/gmail/veneciacalista/ui/drama/ActDrama.java
+++ app/src/main/java/com/gmail/veneciacalista/ui/drama/ActDrama.java
@@ -1,28 +1,41 @@
 package com.gmail.veneciacalista.ui.drama;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.gmail.veneciacalista.R;
+import com.gmail.veneciacalista.firebase.response.Response;
 import com.gmail.veneciacalista.ui.drama.adapter.bigAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigValue;
+import com.google.gson.Gson;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import butterknife.ButterKnife;
 
 public class ActDrama extends AppCompatActivity {
     public List<String> movieList = new ArrayList<>();
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
 
-    @Override //ambil dari parent kek super
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_drama);
         ButterKnife.bind(this);
         setupAdapter();
+        setUpFirebase();
     }
 
     private void setupAdapter() {
@@ -35,5 +48,36 @@ public class ActDrama extends AppCompatActivity {
         recyclerView.setAdapter(mAdapter);
         recyclerView.setNestedScrollingEnabled(false);
     }
+
+    private void setUpFirebase() {
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        defaultKey();
+        mFirebaseRemoteConfig.fetchAndActivate()
+                .addOnCompleteListener(this, new OnCompleteListener<Boolean>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Boolean> task) {
+                        if (task.isSuccessful()) {
+                            boolean updated = task.getResult();
+                            Log.d("taggg", "Config params updated: " + updated);
+                            Toast.makeText(ActDrama.this, "Fetch and activate succeeded",
+                                    Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            Toast.makeText(ActDrama.this, "Fetch failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void defaultKey() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("genre_list", "");
+        mFirebaseRemoteConfig.setDefaults(map);
+        FirebaseRemoteConfigValue m = mFirebaseRemoteConfig.getValue("genre_list");
+        Response json = new Gson().fromJson(m.asString(), Response.class);
+        Log.d("Firebase is fetching", new Gson().toJson(json));
+    }
+
 
 }
